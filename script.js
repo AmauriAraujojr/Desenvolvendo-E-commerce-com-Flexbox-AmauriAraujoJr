ddddb00c2d9f6017aab969219ea780e2d10b9c21
Amauri Araujo <amauriaraujojr@yahoo.com.br>
2022-12-05 16:27:05 -0300

    comerce

---
+++ script.js
@@ -0,0 +1,12 @@
+let valorA=10;
+let valorB=20;
+
+let valorAnteriorA=valorA;
+let valorAtualA=valorB;
+
+let valorAnteriorB=valorB;
+let valorAtualB=valorA;
+
+console.log("Antes a variável'valorA' possuía o valor "+valorA+" , agora possui o valor "+valorAtualA)
+
+console.log("Antes a variável'valorB' possuía o valor "+valorB+" , agora possui o valor "+valorAtualB)
